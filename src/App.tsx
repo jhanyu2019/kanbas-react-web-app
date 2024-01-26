84845e60a9099778d5bb7e29b59cefa827cba977
Jinghan Yu <jinghanyu@jinghandemini.myfiosgateway.com>
2024-01-25 19:43:30 -0500

    made some change

--- src/App.tsx
+++ src/App.tsx
@@ -4,22 +4,15 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+        <header className="App-header">
+          <h1>Welcome to Web Dev CS5610 - 07 Spring 2024</h1>
+          <ul>
+            <li><a href="https://github.com/jhanyu2019/kanbas-ract-web-app/tree/main/public/labs/a1">Assignment 1</a></li>
+            <li><a href="https://github.com/jhanyu2019/kanbas-ract-web-app/tree/main/public/labs/Kanbas">Kanbas Course Home</a></li>
+          </ul>
+        </header>
+      </div>
   );
 }
 

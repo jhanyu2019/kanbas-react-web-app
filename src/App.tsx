e8dd0f4b0990dcb1ad39b90a007484eaf06a7e85
Jinghan Yu <jinghanyu@jinghandemini.myfiosgateway.com>
2024-01-25 21:33:23 -0500

    made change

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App() {
           <h1>Welcome to Web Dev CS5610 - 07 Spring 2024</h1>
           <ul>
             <li><a href="https://main--thriving-gumdrop-9d2290.netlify.app/labs/a1/">Assignment 1</a></li>
-            <li><a href="https://main--thriving-gumdrop-9d2290.netlify.app/labs/kanbas/courses/home/screen">Kanbas Course Home</a></li>
+            <li><a href="labs/kanbas/courses/home/screen">Kanbas Course Home</a></li>
           </ul>
 
         </header>

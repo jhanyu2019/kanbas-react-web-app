1356ab6f9e78bba11518dd57cdcbc44078d615d2
Jinghan Yu <jinghanyu@jinghandemini.myfiosgateway.com>
2024-01-25 22:14:55 -0500

    made change

--- src/App.tsx
+++ src/App.tsx
@@ -5,22 +5,18 @@ import './App.css';
 function App() {
   return (
       <div className="App">
-          <header className="App-header">
-              <img src={logo} className="App-logo" alt="logo" />
-              <p>
-                  Edit <code>src/App.tsx</code> and save to reload.
-              </p>
-              <a
-                  className="App-link"
-                  href="https://reactjs.org"
-                  target="_blank"
-                  rel="noopener noreferrer"
-              >
-                  Learn React
-              </a>
-          </header>
+        <header className="App-header">
+          <h1>Welcome to Web Dev CS5610 - 07 Spring 2024</h1>
+          <ul>
+            <li><a href="/labs/a1/">Assignment 1</a></li>
+            <li><a href="/labs/kanbas/courses/home/screen">Kanbas Course Home</a></li>
+          </ul>
+
+        </header>
       </div>
-  );
+
+
+);
 }
 
 export default App;
